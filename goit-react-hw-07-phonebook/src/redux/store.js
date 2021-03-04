bb8b332a618da3b75c44e79c9ad32368b9fb7bcc
abakarovmaks <abakarovmaks@gmail.com>
2021-03-04 14:25:23 +0200

    Adds script for db

--- goit-react-hw-07-phonebook/src/redux/store.js
+++ goit-react-hw-07-phonebook/src/redux/store.js
@@ -1,8 +1,6 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import {
-  persistStore,
-  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -10,7 +8,7 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 import phoneBookReducer from './phoneBook/phoneBook-reducer';
 
 const middleware = [
@@ -22,25 +20,25 @@ const middleware = [
   logger,
 ];
 
-const phoneBookPersistConfig = {
-  key: 'phoneBook',
-  storage,
-  blacklist: ['filter'],
-};
+// const phoneBookPersistConfig = {
+//   key: 'phoneBook',
+//   storage,
+//   blacklist: ['filter'],
+// };
 
 const store = configureStore({
   reducer: {
-    phoneBook: persistReducer(phoneBookPersistConfig, phoneBookReducer),
+    phoneBook: phoneBookReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
-const commonStore = {
-  store,
-  persistor,
-};
+// const commonStore = {
+//   store,
+//   persistor,
+// };
 
-export default commonStore;
+export default store;
